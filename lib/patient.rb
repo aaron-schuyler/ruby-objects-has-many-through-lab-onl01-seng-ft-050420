284df1acfef8158b46c58bc56c28a90209c01734
Aaron Schuyler <asky28@gmail.com>
2020-05-20 01:04:56 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,4 @@
-class Doctor
+class Patient
   attr_reader :name
   @@all = []
   def initialize(name)
@@ -6,13 +6,13 @@ class Doctor
     @@all << self
   end
   def appointments 
-    Appointment.all.select{|appointment| appointment.doctor == self}
+    Appointment.all.select{|appointment| appointment.patient == self}
   end
-  def patients 
-    self.appointments.collect{|appointment| appointment.patient}.uniq
+  def qqs 
+    self.appointments.collect{|appointment| appointment.qq}.uniq
   end 
-  def new_appointment(name, patient)
-    Appointment.new(name, self, patient)
+  def new_appointment(name, qq)
+    Appointment.new(name, self, qq)
   end
   def self.all
     @@all
